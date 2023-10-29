0b78318b8480f868ef01d179becbf8f430bbfd83
bill <bill.arnold@student.moringaschool.com>
2023-10-29 17:33:12 +0300

    Complete

--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,32 @@
+// import React, { useState, useEffect } from "react";
+
+// function Clock() {
+//   const [time, setTime] = useState(new Date());
+
+//   useEffect(() => {
+//     setInterval(() => {
+//       setTime(new Date());
+//     }, 1000);
+//   }, []);
+
+//   return <div>{time.toString()}</div>;
+// }
+
+// export default Clock;
 import React, { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // Cleanup function to stop the interval when the component is unmounted
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
